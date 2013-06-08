f031d3da2d6dc6638ac360922e3235f7f4b1c64d
kevdashdev <kevdashdev@gmail.com>
2013-06-08 09:36:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedTodoApp::Application.config.secret_token = '468b11338d66756fc1e1661886386fb836cef130203e572c119bfb25b97de45157452ddbe695c234fdb9089fb20416027061e62d78d4c5b1460cd8d7d980f8f2'

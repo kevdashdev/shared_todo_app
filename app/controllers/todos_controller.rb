434a78a8006b88cd69eef3e0339632f2c61eaf04
kevdashdev <kevdashdev@gmail.com>
2013-06-08 12:45:01 +0800

    Finish module 3 lesson 2

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,14 +1,17 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @new_todo = Todo.new
   end
   def delete
 
   end
   def add
-    todo = Todo.create todo_item: params[:todo_text]
+    todo = Todo.create todo_item: params[:todo][:todo_item]
     unless todo.valid?
       flash[:error] = todo.errors.full_messages.join('<br>').html_safe
+    else
+      flash[:success] = "Todo added successfully!"
     end
     redirect_to action: :index
   end
